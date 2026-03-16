4214c3d11de54311179772dd07325add092b44b3
Abel <juanabeldiazperez3@gmail.com>
2026-03-16 19:43:53 -0400

    Avances en SIGEDAA: - Cambios en modelos (Usuario, Atleta, Equipo). - Cambios en controladores (AuthController y flujo de login). - Cambios en frontend (Login, Home y diseño inicial del Dashboard). - Configuración inicial de AppDbContext y manejo de sesiones.

--- SIGEDAA/Controllers/AuthController.cs
+++ SIGEDAA/Controllers/AuthController.cs
@@ -8,7 +8,6 @@ namespace SIGEDAA.Controllers
 {
     public class AuthController : Controller
     {
-
         private readonly AppDbContext _context;
 
         public AuthController(AppDbContext context)
@@ -16,28 +15,26 @@ namespace SIGEDAA.Controllers
             _context = context;
         }
 
-
         // MOSTRAR FORMULARIO LOGIN
         public IActionResult Login()
         {
-            return View();
+            return RedirectToAction("Index", "Home");
         }
-
 
         // PROCESAR LOGIN
         [HttpPost]
         public IActionResult Login(string Email, string password)
         {
-
             var usuario = _context.Usuarios
-                .FirstOrDefault(u => u.Email == Email && u.Password == password);
+                .FirstOrDefault(u =>
+                    u.CorreoElectronico == Email &&
+                    u.ClaveAcceso == password);
 
             if (usuario != null)
             {
-
-                // GUARDAR DATOS EN SESION
-                HttpContext.Session.SetInt32("idusuario", usuario.Id);
-                HttpContext.Session.SetString("usuario", usuario.Nombre);
+                // GUARDAR DATOS EN SESIÓN
+                HttpContext.Session.SetInt32("idusuario", usuario.IdUsuario);
+                HttpContext.Session.SetString("usuario", usuario.NombreCompleto);
                 HttpContext.Session.SetString("rol", usuario.Rol);
 
                 return RedirectToAction("Index", "Dashboard");
@@ -48,14 +45,11 @@ namespace SIGEDAA.Controllers
             return View();
         }
 
-
-        // CERRAR SESION
+        // CERRAR SESIÓN
         public IActionResult Logout()
         {
             HttpContext.Session.Clear();
-
             return RedirectToAction("Login", "Auth");
         }
-
     }
 }
